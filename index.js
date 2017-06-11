b4c009b0429375163a80a7683d2e1b98ff6cecd6
iretd <zanzarah92@gmail.com>
2017-06-11 23:09:38 +0300

    JS-Jail v0.0.0

--- index.js
+++ index.js
@@ -1,60 +1,114 @@
-const fs = require('fs');
-const path = require('path');
+'use strict';
+
+const version = '0.0.0';
+
 const esprima = require('esprima');
 const walk = require( 'esprima-walk' ).walkAddParent;
 const escodegen = require('escodegen');
 
-/**
- * This method is for struggle with some possible infinite loops.
- *
- * @private
- * @param {Object} ast The AST of source code.
- */
-function wrapAllLoops(ast) {
+// Iterations count for detection the infinite loop.
+const LOOP_ITERATIONS_COUNT = 300;
+const INFINITE_LOOP_EXCEPTION_MESSAGE = 'Perhabs there is some infinite loop in the code';
 
-    const loopStatements = ['ForInStatement', 'WhileStatement', 'DoWhileStatement'];
-    const beforeLoopBody = fs.readFileSync(path.join(__dirname, '/injection/loop/body/before'), 'utf8');
-    const innerLoopBody = fs.readFileSync(path.join(__dirname, '/injection/loop/body/inner'), 'utf8');
+module.exports = JSJail();
 
-    walk(ast, function (node) {
+function JSJail() {
+    
+    let t = {};
 
-        var result = loopStatements.indexOf(node.type);
+    t.version = version;
+    t.run = run;
 
-        if (result != -1) {
+    return t;
 
-            node.parent.body.unshift(esprima.parse(beforeLoopBody));
-            node.body.body.unshift(esprima.parse(innerLoopBody));
+    /**
+     * Runs your code into the JS-jail.
+     *
+     * @public
+     * @param code
+     * @param environment
+     */
+    function run(code, environment) {
+
+        const ast = esprima.parse(code);
+
+        if (window) {
+
+            makeStubDeclarations(ast, Object.getOwnPropertyNames(window));
 
         }
 
-    });
+        wrapAllLoops(ast);
+
+        code = escodegen.generate(ast);
+
+        let names = [];
+        let values = [];
+
+        if (environment) {
+
+            names = Object.keys(environment);
+            names.forEach(function (key) {
+
+                values.push(environment[key]);
+
+            });
+
+        }
+
+        // Add code as last parameter of function for apply call.
+        names.push(code);
+
+        const f = Function.apply(this, names);
+
+        f.apply(this, values);
+
+    }
+
+    /**
+     * This method is for struggle with some possible infinite loops.
+     *
+     * @private
+     * @param {Object} ast The AST of source code.
+     */
+    function wrapAllLoops(ast) {
+
+        const loopStatements = ["ForInStatement", "WhileStatement", "DoWhileStatement"];
+        const beforeLoopBody = "var a = 0;";
+        const innerLoopBody = "if (a === " + LOOP_ITERATIONS_COUNT +
+                               ") { throw new Error('" + INFINITE_LOOP_EXCEPTION_MESSAGE + "') }; a++;";
+
+        walk(ast, function (node) {
+
+            var result = loopStatements.indexOf(node.type);
+
+            if (result != -1) {
+
+                node.parent.body.unshift(esprima.parse(beforeLoopBody));
+                node.body.body.unshift(esprima.parse(innerLoopBody));
+
+            }
+
+        });
+
+    }
+
+    /**
+     * Makes stubs of variables for avoid the changing of outside environment which has these variables too.
+     *
+     * @private
+     * @param {Array} variables The name list of variables for stubs.
+     * @param {Object} ast The AST of source code.
+     */
+    function makeStubDeclarations(ast, variables) {
+
+        variables.forEach(function(name) {
+
+            let stub = 'var ' + name + ';';
+            ast.body.unshift(esprima.parse(stub));
+
+        });
+
+    }
 
 }
-
-/**
- * Makes stubs of variables for avoid the changing of outside environment which has these variables too.
- *
- * @private
- * @param {Array} variables The name list of variables for stubs.
- * @param {Object} ast The AST of source code.
- */
-function makeStubDeclarations(ast, variables) {
-
-    variables.forEach(function(name) {
-
-        let stub = 'var ' + name + ';';
-        ast.body.unshift(esprima.parse(stub));
-
-    });
-
-}
-
-const sourceCode = fs.readFileSync(path.join(__dirname, '/injection/example'), 'utf8');
-const ast = esprima.parse(sourceCode);
-
-makeStubDeclarations(ast, ['b','c','d']);
-wrapAllLoops(ast);
-
-const outputCode = escodegen.generate(ast);
-
-console.log(outputCode);
