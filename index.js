5dec020e3f7ece89773c0a843412a4f29a9cd858
iretd <zanzarah92@gmail.com>
2017-06-12 00:18:41 +0300

    add init method

--- index.js
+++ index.js
@@ -13,24 +13,25 @@ const INFINITE_LOOP_EXCEPTION_MESSAGE = 'Perhabs there is some infinite loop in 
 module.exports = JSJail();
 
 function JSJail() {
-    
+
+    let code;
+
     let t = {};
 
     t.version = version;
+    t.init = init;
     t.run = run;
 
     return t;
 
     /**
-     * Runs your code into the JS-jail.
+     * Initializes your code for a further execution into the jail.
      *
-     * @public
-     * @param code
-     * @param environment
+     * @param _code The code which will execute by 'run' method.
      */
-    function run(code, environment) {
+    function init(_code) {
 
-        const ast = esprima.parse(code);
+        const ast = esprima.parse(_code);
 
         if (window) {
 
@@ -42,9 +43,26 @@ function JSJail() {
 
         code = escodegen.generate(ast);
 
+    }
+
+    /**
+     * Runs your code into the JS-jail.
+     *
+     * @public
+     * @param environment
+     */
+    function run(environment) {
+
+        if (!code) {
+
+            throw new Error('At first the code to execute must initialize by init method!');
+
+        }
+
         let names = [];
         let values = [];
 
+        // Separates environment into two arrays: names and values to the following initialization of Function object.
         if (environment) {
 
             names = Object.keys(environment);
@@ -56,7 +74,7 @@ function JSJail() {
 
         }
 
-        // Add code as last parameter of function for apply call.
+        // Add code as the last parameter of function for an apply call.
         names.push(code);
 
         const f = Function.apply(this, names);
@@ -73,7 +91,7 @@ function JSJail() {
      */
     function wrapAllLoops(ast) {
 
-        const loopStatements = ["ForInStatement", "WhileStatement", "DoWhileStatement"];
+        const loopStatements = ["ForOfStatement", "ForStatement", "ForInStatement", "WhileStatement", "DoWhileStatement"];
         const beforeLoopBody = "var a = 0;";
         const innerLoopBody = "if (a === " + LOOP_ITERATIONS_COUNT +
                                ") { throw new Error('" + INFINITE_LOOP_EXCEPTION_MESSAGE + "') }; a++;";
